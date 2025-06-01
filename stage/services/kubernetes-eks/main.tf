8c014e16b3d9055dec2f2ddf54c1da37aacd5e56
steph-nnamani@gmail.com <steph-nnamani@gmail.com>
2025-06-01 13:40:37 -0500

    Modified some codes

--- stage/services/kubernetes-eks/main.tf
+++ stage/services/kubernetes-eks/main.tf
@@ -14,7 +14,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
   # profile = "terraform" # Authenticates with OIDC
 }
 
@@ -37,7 +37,7 @@ data "aws_eks_cluster_auth" "cluster" {
 
 module "eks_cluster" {
   # source = "C:/Users/xtrah/terraform-up-and-running-by-Yev-Brikman/chpt4-reusable-infra-with-terraform-modules/module-example/modules/services/eks-cluster"
-  source = "github.com/steph-nnamani/modules//services/eks-cluster?ref=v2.2.0-eks-cluster"
+  source = "github.com/steph-nnamani/modules//services/eks-cluster?ref=v2.2.2-eks-cluster"
   name = var.cluster_name
 
   min_size     = 1
@@ -56,7 +56,7 @@ module "eks_cluster" {
 
 module "simple_webapp" {
   # source = "C:/Users/xtrah/terraform-up-and-running-by-Yev-Brikman/chpt4-reusable-infra-with-terraform-modules/module-example/modules/services/K8s-app"
-  source = "github.com/steph-nnamani/modules//services/K8s-app?ref=v2.2.0-K8s-app"
+  source = "github.com/steph-nnamani/modules//services/K8s-app?ref=v2.2.2-K8s-app"
   name = var.app_name
 
   image          = "nginxdemos/hello"   #"training/webapp"
